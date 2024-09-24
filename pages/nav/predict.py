3c1b58e8dc4e0dcc5ca140945f550264b480f04c
felipe thaleson <thaleson19@hotmail.com>
2024-09-24 14:32:26 -0300

    Update predict.py

--- pages/nav/predict.py
+++ pages/nav/predict.py
@@ -18,33 +18,24 @@ def show_predict():
         prediction = model.predict(text_tfidf)
         return prediction[0]  # Retorna 0 ou 1
 
-    # Função para verificar se o texto parece ser uma notícia
-    def is_news(text):
-        # Verifica se o texto contém palavras-chave e tem um tamanho razoável
-        keywords = ["notícia", "reportagem", "estudo", "pesquisa", "análise", "informação", "fatos"]
-        return (len(text.split()) > 5 and any(keyword in text.lower() for keyword in keywords))
-
     # Botão de prever
     if st.button('Verificar'):
         if user_input:
-            if not is_news(user_input):
-                st.warning("⚠️ **Por favor, insira um texto que seja uma notícia. O modelo foi treinado para verificar informações jornalísticas.**")
-            else:
-                # Adiciona o efeito de "analisando"
-                with st.spinner('Analisando a notícia... 🔍'):
-                    time.sleep(2)  # Simula um tempo de carregamento
-                    result = predict_fake_news(user_input)
+            # Adiciona o efeito de "analisando"
+            with st.spinner('Analisando a notícia... 🔍'):
+                time.sleep(2)  # Simula um tempo de carregamento
+                result = predict_fake_news(user_input)
                 
-                # Exibe o resultado da previsão
-                if result == 0:
-                    st.error("🛑 **Esta informação provavelmente é: Fake News!**")
-                else:
-                    st.success("✔️ **Esta informação provavelmente é: Verdadeira!**")
+            # Exibe o resultado da previsão
+            if result == 0:
+                st.error("🛑 **Esta informação provavelmente é: Fake News!**")
+            else:
+                st.success("✔️ **Esta informação provavelmente é: Verdadeira!**")
         else:
             st.warning("Por favor, insira um texto antes de verificar!")
 
     # Aviso sobre a precisão do modelo
-    st.markdown(""" 
+    st.markdown("""
         <div style='background-color: #FFFF00; padding: 10px; border-radius: 5px; color: black; font-weight: bold;'>
             <p style='font-size: 14px;'>
                 🔍 Este modelo apresenta uma acurácia de aproximadamente 72% em suas previsões. 
@@ -52,3 +43,5 @@ def show_predict():
             </p>
         </div>
     """, unsafe_allow_html=True)
+
+quero q vc coloque verificação se n for noticia vc coloque aviso dizendo q modelo so prver noticias , tipo se pessoa digia "eu" isso n e noticia
